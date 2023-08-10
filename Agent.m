39ba24d01f7aaeba4af488001dbacec45baf5c1e
Mario Virdis <virdis.mario97@gmail.com>
2023-08-10 18:12:26 +0200

    Initial commit with basic set-get Simulink blocks

---
+++ Agent.m
@@ -0,0 +1,219 @@
+function Agent(block)
+
+%%
+% The setup method is used to set up the basic attributes of the
+% S-function such as ports, parameters, etc. Do not add any other
+% calls to the main body of the function.
+%
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+% Abstract:
+%   Set up the basic characteristics of the S-function block such as:
+%   - Input ports
+%   - Output ports
+%   - Dialog parameters
+%   - Options
+%
+%   Required         : Yes
+%   C MEX counterpart: mdlInitializeSizes
+%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 7;
+block.NumOutputPorts = 0;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = 1;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+block.InputPort(1).DirectFeedthrough = false;
+
+block.InputPort(2).Dimensions        = 1;
+block.InputPort(2).DatatypeID  = 0;  % double
+block.InputPort(2).Complexity  = 'Real';
+block.InputPort(2).DirectFeedthrough = false;
+
+block.InputPort(3).Dimensions        = 1;
+block.InputPort(3).DatatypeID  = 0;  % double
+block.InputPort(3).Complexity  = 'Real';
+block.InputPort(3).DirectFeedthrough = false;
+
+block.InputPort(4).Dimensions        = 1;
+block.InputPort(4).DatatypeID  = 0;  % double
+block.InputPort(4).Complexity  = 'Real';
+block.InputPort(4).DirectFeedthrough = false;
+
+block.InputPort(5).Dimensions        = 1;
+block.InputPort(5).DatatypeID  = 0;  % double
+block.InputPort(5).Complexity  = 'Real';
+block.InputPort(5).DirectFeedthrough = false;
+
+block.InputPort(6).Dimensions        = 1;
+block.InputPort(6).DatatypeID  = 0;  % double
+block.InputPort(6).Complexity  = 'Real';
+block.InputPort(6).DirectFeedthrough = false;
+
+block.InputPort(7).Dimensions        = 1;
+block.InputPort(7).DatatypeID  = 0;  % double
+block.InputPort(7).Complexity  = 'Real';
+block.InputPort(7).DirectFeedthrough = false;
+
+% Override output port properties
+% block.OutputPort(1).Dimensions       = 1;
+% block.OutputPort(1).DatatypeID  = 0; % double
+% block.OutputPort(1).Complexity  = 'Real';
+
+% Register parameters
+block.NumDialogPrms     = 0;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [-1 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+% The MATLAB S-function uses an internal registry for all
+% block methods. You should register all relevant methods
+% (optional and required) as illustrated below. You may choose
+% any suitable name for the methods and implement these methods
+% as local functions within the same file. See comments
+% provided for each function for more information.
+% -----------------------------------------------------------------
+
+block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
+block.RegBlockMethod('InitializeConditions', @InitializeConditions);
+block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+block.RegBlockMethod('Update', @Update);
+block.RegBlockMethod('Derivatives', @Derivatives);
+block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+%%
+% PostPropagationSetup:
+%   Functionality    : Setup work areas and state variables. Can
+%                      also register run-time methods here
+%   Required         : No
+%   C MEX counterpart: mdlSetWorkWidths
+%%
+function DoPostPropSetup(block)
+block.NumDworks = 0;
+  
+%   block.Dwork(1).Name            = 'x1';
+%   block.Dwork(1).Dimensions      = 1;
+%   block.Dwork(1).DatatypeID      = 0;      % double
+%   block.Dwork(1).Complexity      = 'Real'; % real
+%   block.Dwork(1).UsedAsDiscState = true;
+
+
+%%
+%% InitializeConditions:
+%%   Functionality    : Called at the start of simulation and if it is 
+%%                      present in an enabled subsystem configured to reset 
+%%                      states, it will be called when the enabled subsystem
+%%                      restarts execution to reset the states.
+%%   Required         : No
+%%   C MEX counterpart: mdlInitializeConditions
+%%
+function InitializeConditions(block)
+
+%end InitializeConditions
+
+
+%%
+% Start:
+%   Functionality    : Called once at start of model execution. If you
+%                      have states that should be initialized once, this 
+%                      is the place to do it.
+%   Required         : No
+%   C MEX counterpart: mdlStart
+%
+function Start(block)
+
+% block.Dwork(1).Data = 0;
+
+
+
+
+%end Start
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+
+% block.OutputPort(1).Data = block.Dwork(1).Data + block.InputPort(1).Data;
+
+%end Outputs
+
+%%
+% Update:
+%   Functionality    : Called to update discrete states
+%                      during simulation step
+%   Required         : No
+%   C MEX counterpart: mdlUpdate
+%
+function Update(block)
+
+global client
+% block.Dwork(1).Data = block.InputPort(1).Data;
+
+% Send position and attitude data via socket
+data = [block.InputPort(1).Data,...
+        block.InputPort(2).Data,...
+        block.InputPort(3).Data,...
+        block.InputPort(4).Data,...
+        block.InputPort(5).Data,...
+        block.InputPort(6).Data,...
+        block.InputPort(7).Data];
+payload = typecast(data,"uint8");
+header = typecast(uint8([0,0]),"uint8");
+write(client,[header,payload])
+
+%end Update
+
+%%
+%% Derivatives:
+%%   Functionality    : Called to update derivatives of
+%%                      continuous states during simulation step
+%%   Required         : No
+%%   C MEX counterpart: mdlDerivatives
+%%
+function Derivatives(block)
+
+%end Derivatives
+
+%%
+% Terminate:
+%   Functionality    : Called at the end of simulation for cleanup
+%   Required         : Yes
+%   C MEX counterpart: mdlTerminate
+%
+function Terminate(block)
+
+%end Terminate
+
